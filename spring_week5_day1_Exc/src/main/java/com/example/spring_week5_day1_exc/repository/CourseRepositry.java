b28fe041fa1c9eb38b8f1ffcec4921fc4235804f
Hashem Bajabaa <Hashem.bajabaa1@gmail.com>
2023-02-08 18:46:44 +0300

    Delete spring_week5_day1_Exc directory

--- spring_week5_day1_Exc/src/main/java/com/example/spring_week5_day1_exc/repository/CourseRepositry.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_week5_day1_exc.repository;
-
-import com.example.spring_week5_day1_exc.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepositry extends JpaRepository<Course, Integer> {
-}
--- spring_week5_day1_Exc/src/main/java/com/example/spring_week5_day1_exc/repository/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_week5_day1_exc.repository;
-
-import com.example.spring_week5_day1_exc.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student, Integer> {
-}
